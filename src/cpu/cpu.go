784195eff24af497d7a1179cc7822c8709a1a984
ArthurHydr <61481946+ArthurHydr@users.noreply.github.com>
2022-02-13 12:58:39 -0300

    [Update] - cpu/cpu.go (Fix some tabs)

--- src/cpu/cpu.go
+++ src/cpu/cpu.go
@@ -28,7 +28,8 @@ type Chip8 struct {
 	Keys          [16]byte            
 	Draw          bool                
 	Inputflag     bool                
-	InputRegister byte                }
+	InputRegister byte                
+}
 
 func NewCpu() Chip8 {
 	c := Chip8{Pc: 0x200}
@@ -78,13 +79,16 @@ func (c *Chip8) Reset() {
 	for i := 0; i < len(c.Memory); i++ {
 		c.Memory[i] = 0
 	}
-	for i := 0; i < len(c.Stack); i++ {
+	
+  for i := 0; i < len(c.Stack); i++ {
 		c.Stack[i] = 0
 	}
-	for i := 0; i < len(c.V); i++ {
+	
+  for i := 0; i < len(c.V); i++ {
 		c.V[i] = 0
 	}
-	for i := 0; i < len(c.Keys); i++ {
+	
+  for i := 0; i < len(c.Keys); i++ {
 		c.Keys[i] = 0
 	}
 	c.LoadFontSet()
@@ -107,7 +111,8 @@ func (c *Chip8) RunCpuCycle() {
 	opcode := uint16(c.Memory[c.Pc])<<8 | uint16(c.Memory[c.Pc+1])
 	c.Pc = c.Pc + 2
 	switch opcode & 0xF000 {
-	case 0x0000:
+	
+  case 0x0000:
 		switch opcode & 0x000F {
 		case 0x0000:
 			c.ClearDisplay()
@@ -115,148 +120,182 @@ func (c *Chip8) RunCpuCycle() {
 			c.Pc = c.Stack[c.Sp-1]
 			c.Sp = c.Sp - 1
 		}
-	case 0x1000:
+	
+  case 0x1000:
 		c.Pc = opcode & 0x0FFF
-	case 0x2000:
+	
+  case 0x2000:
 		c.Stack[c.Sp] = c.Pc
 		c.Sp = c.Sp + 1
 		c.Pc = opcode & 0x0FFF
-	case 0x3000:
+	
+  case 0x3000:
 		compareTo := byte(opcode & 0x00FF)
 		register := (opcode & 0x0F00) >> 8
 		if c.V[register] == compareTo {
 			c.Pc = c.Pc + 2
 		}
-	case 0x4000:
+	
+  case 0x4000:
 		compareTo := byte(opcode & 0x00FF)
 		register := (opcode & 0x0F00) >> 8
 		if c.V[register] != compareTo {
 			c.Pc = c.Pc + 2
 		}
-	case 0x5000:
+	
+  case 0x5000:
 		registerX := (opcode & 0x0F00) >> 8
 		registerY := (opcode & 0x00F0) >> 4
 		if c.V[registerX] == c.V[registerY] {
 			c.Pc = c.Pc + 2
 		}
-	case 0x6000:
+	
+  case 0x6000:
 		register := byte((opcode & 0x0F00) >> 8)
 		c.V[register] = byte(opcode & 0x00FF)
-	case 0x7000:
+	
+  case 0x7000:
 		register := byte((opcode & 0x0F00) >> 8)
 		value := byte(opcode & 0x00FF)
 		c.V[register] = c.V[register] + value
-	case 0x8000:
+	
+  case 0x8000:
 		switch opcode & 0x000F {
 		case 0x0000:
 			registerX := (opcode & 0x0F00) >> 8
 			registerY := (opcode & 0x00F0) >> 4
 			c.V[registerX] = c.V[registerY]
-		case 0x0001:
+	
+    case 0x0001:
 			registerX := (opcode & 0x0F00) >> 8
 			registerY := (opcode & 0x00F0) >> 4
 			c.V[registerX] = c.V[registerX] | c.V[registerY]
-		case 0x0002:
+		
+    case 0x0002:
 			registerX := (opcode & 0x0F00) >> 8
 			registerY := (opcode & 0x00F0) >> 4
 			c.V[registerX] = c.V[registerX] & c.V[registerY]
-		case 0x0003:
+		
+    case 0x0003:
 			registerX := (opcode & 0x0F00) >> 8
 			registerY := (opcode & 0x00F0) >> 4
 			c.V[registerX] = c.V[registerX] ^ c.V[registerY]
-		case 0x0004:
+		
+    case 0x0004:
 			registerX := byte((opcode & 0x0F00) >> 8)
 			registerY := byte((opcode & 0x00F0) >> 4)
 			c.V[registerX] = c.V[registerX] + c.V[registerY]
-			if uint16(c.V[registerX])+uint16(c.V[registerY]) > 0xFF {
+		
+      if uint16(c.V[registerX])+uint16(c.V[registerY]) > 0xFF {
 				c.V[0xF] = 1
 			} else {
 				c.V[0xF] = 0
 			}
-		case 0x0005:
+		
+    case 0x0005:
 			registerX := (opcode & 0x0F00) >> 8
 			registerY := (opcode & 0x00F0) >> 4
-			if c.V[registerX] > c.V[registerY] {
+			
+      if c.V[registerX] > c.V[registerY] {
 				c.V[0xF] = 1
 			} else {
 				c.V[0xF] = 0
 			}
 			c.V[registerX] = c.V[registerX] - c.V[registerY]
-		case 0x0006:
+		
+    case 0x0006:
 			registerX := (opcode & 0x0F00) >> 8
-			if c.V[registerX]&0x1 == 1 {
+			
+      if c.V[registerX]&0x1 == 1 {
 				c.V[0xF] = 1
 			} else {
 				c.V[0xF] = 0
 			}
 			c.V[registerX] = c.V[registerX] >> 1
-		case 0x0007:
+		
+    case 0x0007:
 			registerX := (opcode & 0x0F00) >> 8
 			registerY := (opcode & 0x00F0) >> 4
-			if c.V[registerY] > c.V[registerX] {
+			
+      if c.V[registerY] > c.V[registerX] {
 				c.V[0xF] = 1
 			} else {
 				c.V[0xF] = 0
 			}
 			c.V[registerX] = c.V[registerY] - c.V[registerX]
-		case 0x000E:
+		
+    case 0x000E:
 			registerX := (opcode & 0x0F00) >> 8
-			if c.V[registerX]&0x80 == 0x80 {
+			
+      if c.V[registerX]&0x80 == 0x80 {
 				c.V[0xF] = 1
 			} else {
 				c.V[0xF] = 0
 			}
 			c.V[registerX] = c.V[registerX] << 1
 		}
-	case 0x9000:
+	
+  case 0x9000:
 		registerX := (opcode & 0x0F00) >> 8
 		registerY := (opcode & 0x00F0) >> 4
-		if c.V[registerX] != c.V[registerY] {
+		
+    if c.V[registerX] != c.V[registerY] {
 			c.Pc = c.Pc + 2
 		}
-	case 0xA000:
+	
+  case 0xA000:
 		c.I = (opcode & 0x0FFF)
-	case 0xB000:
+	
+  case 0xB000:
 		c.Pc = (opcode & 0x0FFF) + uint16(c.V[0x0])
-	case 0xC000:
+	
+  case 0xC000:
 		registerX := (opcode & 0x0F00) >> 8
 		value := byte(opcode & 0x00FF)
 		rand.Seed(time.Now().Unix())
 		c.V[registerX] = byte(rand.Intn(256)) & value
-	case 0xD000:
+	
+  case 0xD000:
 		registerX := (opcode & 0x0F00) >> 8
 		registerY := (opcode & 0x00F0) >> 4
 		nibble := byte(opcode & 0x000F)
 		x := c.V[registerX]
 		y := c.V[registerY]
 		c.V[0xF] = 0x00
-		for i := y; i < y+nibble; i++ {
+		
+    for i := y; i < y+nibble; i++ {
 			for j := x; j < x+8; j++ {
 				bit := (c.Memory[c.I+uint16(i-y)] >> (7 - j + x)) & 0x01
 				xIndex, yIndex := j, i
 				if j >= width {
 					xIndex = j - width
 				}
-				if i >= height {
+				
+        if i >= height {
 					yIndex = i - height
 				}
-				if bit == 0x01 && c.Display[yIndex][xIndex] == 0x01 {
+				
+        if bit == 0x01 && c.Display[yIndex][xIndex] == 0x01 {
 					c.V[0xF] = 0x01
 				}
 				c.Display[yIndex][xIndex] = c.Display[yIndex][xIndex] ^ bit
 			}
 		}
 		c.Draw= true
-	case 0xE000:
+	
+  case 0xE000:
 		switch opcode & 0x00FF {
 		case 0x009E:
 			register := (opcode & 0x0F00) >> 8
-			if c.Keys[c.V[register]] == 0x01 {
+			
+      if c.Keys[c.V[register]] == 0x01 {
 				c.Pc = c.Pc + 2
 			}
-		case 0x00A1:
+		
+    case 0x00A1:
 			register := (opcode & 0x0F00) >> 8
-			if c.Keys[c.V[register]] == 0x00 {
+			
+      if c.Keys[c.V[register]] == 0x00 {
 				c.Pc = c.Pc + 2
 			}
 		}
@@ -265,36 +304,46 @@ func (c *Chip8) RunCpuCycle() {
 		case 0x007:
 			register := (opcode & 0x0F00) >> 8
 			c.V[register] = c.DelayTimer
-		case 0x0015:
+		
+    case 0x0015:
 			register := (opcode & 0x0F00) >> 8
 			c.DelayTimer = c.V[register]
-		case 0x0018:
+		
+    case 0x0018:
 			register := (opcode & 0x0F00) >> 8
 			c.SoundTimer = c.V[register]
-		case 0x000A:
+		
+    case 0x000A:
 			register := (opcode & 0x0F00) >> 8
 			c.Inputflag = true
 			c.InputRegister = byte(register)
-		case 0x001E:
+		
+    case 0x001E:
 			register := (opcode & 0x0F00) >> 8
 			c.I = c.I + uint16(c.V[register])
-		case 0x0029:
+		
+    case 0x0029:
 			register := (opcode & 0x0F00) >> 8
 			c.I = uint16(c.V[register] * 0x5)
-		case 0x0033:
+		
+    case 0x0033:
 			register := (opcode & 0x0F00) >> 8
 			number := c.V[register]
 			c.Memory[c.I] = (number / 100) % 10
 			c.Memory[c.I+1] = (number / 10) % 10
 			c.Memory[c.I+2] = number % 10
-		case 0x0055:
+		
+    case 0x0055:
 			register := (opcode & 0x0F00) >> 8
-			for i := uint16(0x00); i <= register; i++ {
+			
+      for i := uint16(0x00); i <= register; i++ {
 				c.Memory[c.I+i] = c.V[i]
 			}
-		case 0x0065:
+		
+    case 0x0065:
 			register := (opcode & 0x0F00) >> 8
-			for i := uint16(0x00); i <= register; i++ {
+			
+      for i := uint16(0x00); i <= register; i++ {
 				c.V[i] = c.Memory[c.I+i]
 			}
 		}
